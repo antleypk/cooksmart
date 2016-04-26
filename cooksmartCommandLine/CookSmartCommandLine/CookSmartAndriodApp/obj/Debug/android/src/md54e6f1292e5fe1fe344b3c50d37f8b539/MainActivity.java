191763003a0a80524680b0a07e12c57e6495ccba
Vic Finney <Vic Finney@LAPTOP-UQGR06BE>
2016-04-26 12:05:38 -0400

    Publishing Andriod App developement branch.

---
+++ cooksmartCommandLine/CookSmartCommandLine/CookSmartAndriodApp/obj/Debug/android/src/md54e6f1292e5fe1fe344b3c50d37f8b539/MainActivity.java
@@ -0,0 +1,46 @@
+package md54e6f1292e5fe1fe344b3c50d37f8b539;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("CookSmartAndriodApp.MainActivity, CookSmartAndriodApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("CookSmartAndriodApp.MainActivity, CookSmartAndriodApp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
