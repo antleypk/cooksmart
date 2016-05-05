b4708e529b8bf2d3cf3f4020c668891253102c9d
Vic Finney <Vic Finney@LAPTOP-UQGR06BE>
2016-05-04 20:32:44 -0400

    Deleted the Android App and started an actual Xamarin project in it's place.  Functionally this is the same as the Android project that I deleted.  Pratically this will allow me to write code using XAML

---
+++ cooksmartCommandLine/CookSmartCommandLine/CookSmartXamarinApp/CookSmartXamarinApp/Views/Index.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CookSmartXamarinApp.Views
+{
+    public partial class Index : ContentPage
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+    }
+}
